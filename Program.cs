819c7ef0a6c094ecf06b1d82503a2375a42a3515
Akhil <vmakhil04@gmail.com>
2023-11-23 10:52:09 +0530

    Second commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Program  
+    {          
+        
+        private static int ExcelColumnNameToNumber(string columnName)
+{
+    if (string.IsNullOrEmpty(columnName)) throw new ArgumentNullException("columnName");
+
+    columnName = columnName.ToUpperInvariant();
+
+    int sum = 0;
+
+    for (int i = 0; i < columnName.Length; i++)
+    {
+        sum *= 26;
+        sum += (columnName[i] - 'A' + 1);
+    }
+
+    return sum;
+}
+        
+        static void Main(string[] args)  
+        {  
+            string column;  
+            Console.WriteLine("please enter the excel column");  
+            column =(Console.ReadLine());
+           int value= ExcelColumnNameToNumber(column);
+           Console.WriteLine("Column value is :" + value.ToString());   
+           
+        }  
+    }
